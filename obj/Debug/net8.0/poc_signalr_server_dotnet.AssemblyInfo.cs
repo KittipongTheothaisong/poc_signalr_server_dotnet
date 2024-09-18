df7f0eb3bc40593ac6f16a66b89a3cea334d1efb
Kittipong Theothaisong <kittipong.t@bafs-id.co.th>
2024-09-18 14:44:53 +0700

    changed to backslash

--- obj/Debug/net8.0/poc_signalr_server_dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/poc_signalr_server_dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("poc_signalr_server_dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a54d4eb522149a2e3be680fbfe563b87683d74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d10b7241e1ab8ec9de949ce82bb125cae489ec6")]
 [assembly: System.Reflection.AssemblyProductAttribute("poc_signalr_server_dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("poc_signalr_server_dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
